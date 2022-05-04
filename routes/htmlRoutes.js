b2cd882582d40eec6caa73bda858e3631979066c
Hailey Bates <hailey.bates001@gmail.com>
2022-05-04 02:03:18 -0400

    Worked through html route on hw startup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,15 @@
 // require the path and router items needed
+const path = require('path');
+const router = require('express').Router();
 
 //need to add the router.get functionality for each of the html pages
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/notes.html"));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/index.html"));
+});
 
 //todo export the routes
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 //todo add required routes for api/html
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 //initialize the app and create a port
 const app = express();
 const PORT =process.env.PORT || 3001;
@@ -10,6 +11,8 @@ const PORT =process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 //start the server on the port
 app.listen(PORT, () => console.log('This port is listening at ${PORT}'));
